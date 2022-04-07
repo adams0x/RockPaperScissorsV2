34113aba8019ad2d84990cab91a3e2547e4a9882
adams0x <vadams.email@gmail.com>
2022-04-07 10:57:26 +0100

    Added attempts counter

--- scripts.js
+++ scripts.js
@@ -1,35 +1,36 @@
 
 var computersChosenResult;
-var winningResult;
+var userIsWinner;
+var userAttempts = 0;
 
 function rockClicked(){
     calcWinner(0, computerSelection());
-    gameResultUpdate(winningResult);
+    gameResultUpdate(userIsWinner);
 }
 
 
 function paperClicked(){
     calcWinner(1, computerSelection());
-    gameResultUpdate(winningResult);
+    gameResultUpdate(userIsWinner);
 }
 
 
 function scissorsClicked(){
     calcWinner(2, computerSelection());
-    gameResultUpdate(winningResult);
+    gameResultUpdate(userIsWinner);
 }
 
 function gameResultUpdate(isWinner){
     if(isWinner==1){
-        document.getElementById("idPlayResult").innerHTML="Well done, you win ! Computer chose " + computersChosenResult;
+        document.getElementById("idPlayResult").innerHTML="Attempt #" + userAttempts +": Well done, you win ! Computer chose " + computersChosenResult;
         document.getElementById("idPlayResult").style.backgroundColor = "chartreuse";
     }
     else if(isWinner==0){
-        document.getElementById("idPlayResult").innerHTML="Its a draw, computer chose " + computersChosenResult;
+        document.getElementById("idPlayResult").innerHTML="Attempt #" + userAttempts +": Its a draw, computer chose " + computersChosenResult;
         document.getElementById("idPlayResult").style.backgroundColor = "orange";
     }
     else{
-        document.getElementById("idPlayResult").innerHTML="Sorry, you lost, computer chose " + computersChosenResult;
+        document.getElementById("idPlayResult").innerHTML="Attempt #" + userAttempts +": Sorry, you lost, computer chose " + computersChosenResult;
         document.getElementById("idPlayResult").style.backgroundColor = "crimson";
     }
 
@@ -43,6 +44,8 @@ function computerSelection(){
 
 
 function calcWinner(userSelection, computerSelection){
+
+    userAttempts++;
 
     if(computerSelection == 0){
         computersChosenResult="rock";
@@ -59,20 +62,20 @@ function calcWinner(userSelection, computerSelection){
 
 
     if(userSelection == computerSelection){
-        winningResult = 0;
+        userIsWinner = 0;
     }
     else{
         if(userSelection==0 && computerSelection==2){
-            winningResult = 1;
+            userIsWinner = 1;
         }
         else if(userSelection==1 && computerSelection==0){
-            winningResult = 1;
+            userIsWinner = 1;
         }
         else if(userSelection==2 && computerSelection==1){
-            winningResult = 1;
+            userIsWinner = 1;
         }
         else
-        winningResult = 2;
+        userIsWinner = 2;
     }
 
 }
